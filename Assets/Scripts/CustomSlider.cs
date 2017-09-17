236c269130576a4b13ddaa4cb432a109f7f615ac
mljustinli <ml.justin.li@gmail.com>
2017-09-17 18:39:34 -0400

    Make custom sliders with two knobs and buttons for fine tuning the values of the knobs

--- Assets/Scripts/CustomSlider.cs
+++ Assets/Scripts/CustomSlider.cs
@@ -17,6 +17,19 @@ public class CustomSlider : MonoBehaviour {
 	private bool heldOnKnob1;
 	private bool heldOnKnob2;
 
+	public int knob1Value;
+	public int knob2Value;
+
+	private GameObject upLeft;
+	private GameObject downLeft;
+	private GameObject upRight;
+	private GameObject downRight;
+
+	private GameObject[] adjustButtons;
+
+	private int totalMeasures = 200;
+
+
 	// Use this for initialization
 	void Start () {
 		background = transform.GetChild (0).gameObject;
@@ -32,6 +45,19 @@ public class CustomSlider : MonoBehaviour {
 
 		heldOnKnob1 = false;
 		heldOnKnob2 = false;
+
+		knob1Value = 2;
+		knob2Value = 80;
+		moveLeftKnob (knob1Value);
+		moveRightKnob (knob2Value);
+
+
+		upLeft = transform.GetChild (1).gameObject;
+		downLeft = transform.GetChild (2).gameObject;
+		upRight = transform.GetChild (3).gameObject;
+		downRight = transform.GetChild (4).gameObject;
+
+		adjustButtons = new GameObject[] { upLeft, downLeft, upRight, downRight };
 	}
 	
 	// Update is called once per frame
@@ -44,27 +70,99 @@ public class CustomSlider : MonoBehaviour {
 
 		if (heldOnKnob1 || (Input.GetMouseButton (0) && RectTransformUtility.RectangleContainsScreenPoint (knob1.transform.GetComponent<Image> ().rectTransform, (Vector2)Input.mousePosition))) {
 			if (!heldOnKnob2) {
-				Debug.Log ("click is in bounds");
+//				Debug.Log ("click is in bounds");
+//				heldOnKnob1 = true;
+////				knob1.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, bgXLeft, bgXRight), 0, 0);
+////				knob1.transform.localPosition = new Vector3 (Mathf.Clamp(knob1.transform.localPosition.x, -bgWidth/2, knob2.transform.localPosition.x - 20), 0, 0);
+//				moveLeftKnob(Input.mousePosition.x);
+
 				heldOnKnob1 = true;
-				knob1.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, bgXLeft, bgXRight), 0, 0);
-				knob1.transform.localPosition = new Vector3 (Mathf.Clamp(knob1.transform.localPosition.x, -bgWidth/2, knob2.transform.localPosition.x - 20), 0, 0);
+				Vector3 pos = knob1.transform.position;
+				knob1.transform.position = new Vector3 (Input.mousePosition.x, pos.y, 0);
+				int value = Mathf.RoundToInt (((knob1.transform.localPosition.x + bgWidth / 2) / bgWidth) * totalMeasures);
+//				knob1Value = value;
+//				knob1Value = Mathf.Clamp (knob1Value, 1, knob2Value);
+				moveLeftKnob(value);
 			}
 		}
 
 		if (heldOnKnob2 || (Input.GetMouseButton (0) && RectTransformUtility.RectangleContainsScreenPoint (knob2.transform.GetComponent<Image> ().rectTransform, (Vector2)Input.mousePosition))) {
 			if (!heldOnKnob1) {
-				Debug.Log ("click is in bounds");
+//				Debug.Log ("click is in bounds");
+//				heldOnKnob2 = true;
+////				knob2.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, bgXLeft, bgXRight), 0, 0);
+////				knob2.transform.localPosition = new Vector3 (Mathf.Clamp(knob2.transform.localPosition.x, knob1.transform.localPosition.x + 20, bgWidth/2), 0, 0);
+//				moveRightKnob(Input.mousePosition.x);
+
 				heldOnKnob2 = true;
-				knob2.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, bgXLeft, bgXRight), 0, 0);
-				knob2.transform.localPosition = new Vector3 (Mathf.Clamp(knob2.transform.localPosition.x, knob1.transform.localPosition.x + 20, bgWidth/2), 0, 0);
+				Vector3 pos = knob2.transform.position;
+				knob2.transform.position = new Vector3 (Input.mousePosition.x, pos.y, 0);
+				int value = Mathf.RoundToInt (((knob2.transform.localPosition.x + bgWidth / 2) / bgWidth) * totalMeasures);
+//				knob2Value = value;
+//				knob2Value = Mathf.Clamp (knob2Value, knob1Value + 1, totalMeasures);
+				moveRightKnob(value);
 			}
 		}
+
+
+//		knob1Value = Mathf.RoundToInt (((knob1.transform.localPosition.x + bgWidth / 2) / bgWidth) * totalMeasures);
+//		knob2Value = Mathf.RoundToInt (((knob2.transform.localPosition.x + bgWidth / 2) / bgWidth) * totalMeasures);
+
+//		float distance = bgWidth / totalMeasures;
+//		Debug.Log (distance);
+
+		for (int i = 0; i < adjustButtons.Length; i++) {
+			if (Input.GetMouseButtonDown(0) && RectTransformUtility.RectangleContainsScreenPoint(adjustButtons[i].transform.GetComponent<Image>().rectTransform, (Vector2)Input.mousePosition)) {
+				switch(i) {
+				case 0:
+//					knob1Value++;
+					moveLeftKnob(++knob1Value);
+					break;
+				case 1:
+//					knob1Value--;
+					moveLeftKnob(--knob1Value);
+					break;
+				case 2:
+//					knob2Value++;
+					moveRightKnob(++knob2Value);
+					break;
+				case 3:
+					//					knob2Value--;
+					moveRightKnob(--knob2Value);
+					break;
+				}
+			}
+		}
+
+////		if (heldOnKnob1) {
+//			knob1Value = Mathf.Clamp (knob1Value, 1, knob2Value - 1);
+//			Vector3 knob1Pos = knob1.transform.localPosition;
+//			knob1.transform.localPosition = new Vector3 ((knob1Value / (totalMeasures + 0.0f)) * bgWidth - bgWidth / 2, knob1Pos.y, 0);
+////		}
+//
+////		if (heldOnKnob2) {
+//			knob2Value = Mathf.Clamp (knob2Value, knob1Value + 1, totalMeasures);
+//			Vector3 knob2Pos = knob2.transform.localPosition;
+//			knob2.transform.localPosition = new Vector3 ((knob2Value / (totalMeasures + 0.0f)) * bgWidth - bgWidth / 2, knob2Pos.y, 0);
+////		}
+
+
 
 		if (Input.GetMouseButtonUp (0)) {
 			heldOnKnob1 = false;
 			heldOnKnob2 = false;
 		}
+	}
 
+	void moveLeftKnob (int newVal) {
+		knob1Value = Mathf.Clamp (newVal, 1, knob2Value - 1);
+		Vector3 knob1Pos = knob1.transform.localPosition;
+		knob1.transform.localPosition = new Vector3 ((knob1Value / (totalMeasures + 0.0f)) * bgWidth - bgWidth / 2, knob1Pos.y, 0);
+	}
 
+	void moveRightKnob (int newVal) {
+		knob2Value = Mathf.Clamp (newVal, knob1Value + 1, totalMeasures);
+		Vector3 knob2Pos = knob2.transform.localPosition;
+		knob2.transform.localPosition = new Vector3 ((knob2Value / (totalMeasures + 0.0f)) * bgWidth - bgWidth / 2, knob2Pos.y, 0);
 	}
 }
